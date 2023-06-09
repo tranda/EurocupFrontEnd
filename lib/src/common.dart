b36ccbf5849a614d52904f8c40ad5f4a05ff6653
Zoran Trandafilovic <tranda@gmail.com>
2023-06-09 22:15:53 +0200

    added test account for dev

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -1,5 +1,6 @@
 import 'package:eurocup_frontend/src/model/athlete/athlete.dart';
 import 'package:eurocup_frontend/src/model/event/event.dart';
+import 'package:eurocup_frontend/src/model/race/discipline.dart';
 import 'package:flutter/material.dart';
 
 import 'model/user.dart';
@@ -7,12 +8,16 @@ import 'model/user.dart';
 String imagesPath = 'assets/images/';
 
 var TEST = true;
-var testUser = "mymail@gmail.com";
+var ADMINTEST = true;
+var testUser = "test@gmail.com";
 var testPassword = "12345678";
+var adminUser = "tranda@gmail.com";
+var adminPassword = "12345678";
 
 User currentUser = User();
 String token = '';
 List<Competition> competitions = [];
+List<Discipline> disciplines = [];
 
 Athlete currentAthlete = Athlete();
 const String imagePrefix = 'events.motion.rs/photos';
