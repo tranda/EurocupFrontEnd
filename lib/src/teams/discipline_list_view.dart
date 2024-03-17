ae5d64b7f931273f5206d9a55a7495e09eab9d20
Zoran Trandafilovic <tranda@gmail.com>
2024-03-17 23:46:43 +0100

    improvement on discipline registration, multiple registration

--- lib/src/teams/discipline_list_view.dart
+++ lib/src/teams/discipline_list_view.dart
@@ -15,6 +15,7 @@ class DisciplineListView extends StatefulWidget {
 class _CrewListViewState extends State<DisciplineListView> {
   bool locked = false;
   List<int> registeredDisciplines = [];
+  bool changes = false;
 
   @override
   void initState() {
@@ -25,124 +26,187 @@ class _CrewListViewState extends State<DisciplineListView> {
     locked = (currentUser.accessLevel! > 0) && (currentUser.accessLevel! < 3);
   }
 
+  Future<bool> _onWillPop() async {
+    return (!changes
+            ? true
+            : await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: null, // const Text('Are you sure?'),
+                  content: Text(
+                    "You will loose Your changes!",
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: const Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+              )) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as Map;
     final teamId = args['teamId'];
     final teamName = args['teamName'];
-    return Scaffold(
-      appBar: appBarWithAction(
-          locked
-              ? () {}
-              : () {
-                  print(registeredDisciplines);
-                  setState(() {});
-                  api.registerCrews(teamId, registeredDisciplines).then((v) {
-                    setState(() {});
-                  }).catchError((error) {
-                    print('Error creating team: $error');
-                  });
-                },
-          title: teamName,
-          icon: Icons.save),
-      body: Container(
-        // decoration: const BoxDecoration(
-        //     image: DecorationImage(
-        //         image: AssetImage('assets/images/bck.jpg'), fit: BoxFit.cover)),
-        child: FutureBuilder(
-          future: api.getTeamDisciplines(teamId, EVENTID),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasData) {
-              final teamDisciplines = snapshot.data!;
-              print('# of Disciplines: ${disciplines.length}');
-              print('# of registered Disciplines: ${teamDisciplines.length}');
-              return ListView.builder(
-                itemCount: disciplines.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final discipline = disciplines[index];
-                  final inactiveStatus =
-                      discipline.status == "inactive" ? "(INACTIVE)" : "";
-                  var competition = competitions.firstWhere(
-                      (element) => element.id == discipline.eventId);
-                  var eventName = '${competition.name!} ${competition.year}';
-                  var eventColor = competitionColor[discipline.eventId! - 1];
-                  var registered = false;
-                  for (var element in teamDisciplines) {
-                    if (element.discipline!.id == discipline.id) {
-                      registered = true;
-                      registeredDisciplines.add(discipline.id!);
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: appBarWithAction(
+            locked
+                ? () {}
+                : () {
+                    if (changes) {
+                      openDialog().then((value) {
+                        if (value == true) {
+                          api
+                              .registerCrews(teamId, registeredDisciplines)
+                              .then((v) {
+                            Navigator.of(context).pop();
+                          }).catchError((error) {
+                            print('Error creating team: $error');
+                          });
+                        } else {}
+                      }).catchError((error) {
+                        print('Error opening dialog: $error');
+                      });
                     }
-                  }
-                  return Column(
-                    children: [
-                      ListTile(
-                          tileColor: eventColor,
-                          leading: Text(eventName),
-                          title: Text(
-                            "${discipline.getDisplayName()} $inactiveStatus",
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
-                          trailing: Visibility(
-                            visible: true, // discipline.status == "active",
-                            child: StatefulBuilder(builder: (context, setter) {
-                              return Checkbox(
-                                  value: registeredDisciplines
-                                      .contains(discipline.id!), // registered,
-                                  onChanged: (value) {
-                                    if (value != null && !locked) {
-                                      if (value) {
-                                        registeredDisciplines
-                                            .add(discipline.id!);
-                                      } else {
-                                        registeredDisciplines
-                                            .remove(discipline.id!);
+                  },
+            title: teamName,
+            icon: Icons.save),
+        body: Container(
+          // decoration: const BoxDecoration(
+          //     image: DecorationImage(
+          //         image: AssetImage('assets/images/bck.jpg'), fit: BoxFit.cover)),
+          child: FutureBuilder(
+            future: api.getTeamDisciplines(teamId, EVENTID),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasData) {
+                final teamDisciplines = snapshot.data!;
+                print('# of Disciplines: ${disciplines.length}');
+                print('# of registered Disciplines: ${teamDisciplines.length}');
+                return ListView.builder(
+                  itemCount: disciplines.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final discipline = disciplines[index];
+                    final inactiveStatus =
+                        discipline.status == "inactive" ? "(INACTIVE)" : "";
+                    var competition = competitions.firstWhere(
+                        (element) => element.id == discipline.eventId);
+                    var eventName = '${competition.name!} ${competition.year}';
+                    var eventColor = competitionColor[discipline.eventId! - 1];
+                    var registered = false;
+                    for (var element in teamDisciplines) {
+                      if (element.discipline!.id == discipline.id) {
+                        registered = true;
+                        registeredDisciplines.add(discipline.id!);
+                      }
+                    }
+                    return Column(
+                      children: [
+                        ListTile(
+                            tileColor: eventColor,
+                            leading: Text(eventName),
+                            title: Text(
+                              "${discipline.getDisplayName()} $inactiveStatus",
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                            trailing: Visibility(
+                              visible: true, // discipline.status == "active",
+                              child:
+                                StatefulBuilder(builder: (context, setter) {
+                                return Checkbox(
+                                    value: registeredDisciplines.contains(
+                                        discipline.id!), // registered,
+                                    onChanged: (value) {
+                                      if (value != null && !locked) {
+                                        if (value) {
+                                          registeredDisciplines
+                                              .add(discipline.id!);
+                                          changes = true;
+                                        } else {
+                                          registeredDisciplines
+                                              .remove(discipline.id!);
+                                          changes = true;
+                                        }
+                                        setter(() {});
                                       }
-                                      setter(() {});
                                     }
-                                  }
-                                  // (value) {
-                                  //         if (value != null) {
-                                  //           if (value) {
-                                  //             print('register for ');
-                                  //             api
-                                  //                 .registerCrew(
-                                  //                     teamId, discipline.id!)
-                                  //                 .then((value) {
-                                  //               setState(() {});
-                                  //             });
-                                  //           } else {
-                                  //             print('unregister for ');
-                                  //             api
-                                  //                 .unregisterCrew(
-                                  //                     teamId, discipline.id!)
-                                  //                 .then((value) {
-                                  //               setState(() {});
-                                  //             });
-                                  //           }
-                                  //           setState(() {});
-                                  //         }
-                                  //       }
-                                  );
-                            }),
-                          )),
-                      const Divider(
-                        height: 4,
-                      ),
-                      const Divider(
-                        height: smallSpace,
-                      )
-                    ],
-                  );
-                },
-              );
-            }
-            return (const Text('No data'));
-          },
+                                    // (value) {
+                                    //         if (value != null) {
+                                    //           if (value) {
+                                    //             print('register for ');
+                                    //             api
+                                    //                 .registerCrew(
+                                    //                     teamId, discipline.id!)
+                                    //                 .then((value) {
+                                    //               setState(() {});
+                                    //             });
+                                    //           } else {
+                                    //             print('unregister for ');
+                                    //             api
+                                    //                 .unregisterCrew(
+                                    //                     teamId, discipline.id!)
+                                    //                 .then((value) {
+                                    //               setState(() {});
+                                    //             });
+                                    //           }
+                                    //           setState(() {});
+                                    //         }
+                                    //       }
+                                    );
+                              }),
+                            )),
+                        const Divider(
+                          height: 4,
+                        ),
+                        const Divider(
+                          height: smallSpace,
+                        )
+                      ],
+                    );
+                  },
+                );
+              }
+              return (const Text('No data'));
+            },
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool?> openDialog() async {
+    return await showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+                title: null, // const Text(""),
+                content: Text(
+                  "Save Your changes?",
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+                actions: <Widget>[
+                  TextButton(onPressed: cancel, child: const Text("Cancel")),
+                  TextButton(onPressed: submit, child: const Text("OK")),
+                ]));
+  }
+
+  void cancel() {
+    Navigator.of(context).pop(false);
+  }
+
+  void submit() {
+    Navigator.of(context).pop(true);
   }
 }
